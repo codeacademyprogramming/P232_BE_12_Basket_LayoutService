eb91bf913cdd44761f010d59e33f14a0c7ee8487
eaghayev <eaghayevv@gmail.com>
2023-04-17 17:53:21 +0400

    project added

---
+++ Pustok/Pustok/Controllers/BookController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Pustok.DAL;
+using Pustok.ViewModels;
+
+namespace Pustok.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly PustokDbContext _context;
+
+        public BookController(PustokDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetBookModal(int id)
+        {
+            var book = _context.Books
+                .Include(x=>x.Genre)
+                .Include(x => x.Author)
+                .Include(x => x.BookImages)
+                .FirstOrDefault(x=>x.Id == id);
+
+            return PartialView("_BookModalPartial",book);
+        }
+
+        public IActionResult AddToBasket(int id)
+        {
+            List<BasketItemViewModel> basketItems;
+            var basket = HttpContext.Request.Cookies["basket"];
+
+            if (basket == null)
+                basketItems = new List<BasketItemViewModel>();
+            else
+                basketItems = JsonConvert.DeserializeObject<List<BasketItemViewModel>>(basket);
+
+            var wantedBook = basketItems.FirstOrDefault(x => x.BookId == id);
+            
+            if (wantedBook == null)
+                basketItems.Add(new BasketItemViewModel { Count = 1, BookId = id });
+            else
+                wantedBook.Count++;
+
+
+            HttpContext.Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketItems));
+            return RedirectToAction("index", "home");
+        }
+
+        public IActionResult ShowBasket()
+        {
+            var basket = HttpContext.Request.Cookies["basket"];
+            var basketItems = JsonConvert.DeserializeObject<List<BasketItemViewModel>>(basket);
+
+
+            return Json(basketItems);
+        }
+
+    }
+}
---
+++ Pustok/Pustok/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using Pustok.DAL;
+using Pustok.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<PustokDbContext>(opt =>
+{
+    opt.UseSqlServer("Server=DESKTOP-BU4GQ1K\\SQLEXPRESS;Database=P232Pustok;Trusted_Connection=True");
+});
+
+//builder.Services.AddSingleton<LayoutService>();
+builder.Services.AddScoped<LayoutService>();
+//builder.Services.AddTransient<LayoutService>();
+
+//builder.Services.AddSession(opt =>
+//{
+//    //opt.IdleTimeout = TimeSpan.FromSeconds(5);
+//});
+
+builder.Services.AddCookiePolicy(opts => {
+    opts.CheckConsentNeeded = ctx => false;
+    opts.OnAppendCookie = ctx => {
+        ctx.CookieOptions.Expires = DateTimeOffset.UtcNow.AddDays(14);
+    };
+});
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+//app.UseSession();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
